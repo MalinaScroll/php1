b359c134bb91bd788bd2a81a785ec6298da8a91a
Malina De Jonge <Malina.ScrollingIn@gmail.com>
2019-10-15 10:26:23 +0200

    added css file for preferable background

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <link rel="stylesheet" href="main.css">
+    <title>PHP by Malina</title>
   </head>
   <body>
     <?php
-    $phrase = "Hello World!";
+    echo "<h1 id='title'>Malina's php page</h1><hr>";
+    $phrase = "<h3>Hello World!</h3><br>";
       echo $phrase;
      ?>
   </body>
