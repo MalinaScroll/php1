348aba6648054416847dd63dc4985bae12bdf2ac
Malina De Jonge <Malina.ScrollingIn@gmail.com>
2019-10-15 11:12:06 +0200

    added favicon and page description

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="main.css">
+    <link rel="icon" type="image/png" href="fav.png">
     <title>PHP by Malina</title>
   </head>
   <body>
     <?php
     echo "<h1 id='title'>Malina's php page</h1><hr>";
-    $phrase = "<h3>Hello World!</h3><br>";
+    $phrase = "Hello World!<br>";
       echo $phrase;
+      echo "This page is mainly used as a template for future php sites";
      ?>
   </body>
 </html>
